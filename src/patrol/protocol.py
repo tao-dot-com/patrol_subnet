209310cb4978bd811dba3b441f736b21a1ea87b4
Jack Foster <jack@tensora.com>
2025-04-04 10:56:35 +0100

    Updating protocol models

--- src/patrol/protocol.py
+++ src/patrol/protocol.py
@@ -3,26 +3,6 @@ import bittensor as bt
 from dataclasses import dataclass
 from typing import List, Optional
 
-class MinerPingSynapse(bt.Synapse):
-    """
-    A simple ping synapse that inherits from bt.Synapse.
-    This synapse handles ping request and response communication between
-    the miner and the validator.
-    """
-    is_available: typing.Optional[bool] = False
-
-class PatrolSynapse(bt.Synapse):
-    """
-    A simple event graph protocol that inherits from bt.Synapse.
-    This protocol handles event graph request and response communication between
-    the miner and the validator.
-    """
-
-    target: typing.Optional[str] = None
-    target_block_number: typing.Optional[int] = None
-
-    subgraph_output: typing.Optional[dict] = None
-
 @dataclass
 class TransferEvidence:
     rao_amount: int
@@ -64,3 +44,23 @@ class GraphPayload:
     nodes: List[Node]
     edges: List[Edge]
 
+
+class MinerPingSynapse(bt.Synapse):
+    """
+    A simple ping synapse that inherits from bt.Synapse.
+    This synapse handles ping request and response communication between
+    the miner and the validator.
+    """
+    is_available: typing.Optional[bool] = False
+
+class PatrolSynapse(bt.Synapse):
+    """
+    A simple event graph protocol that inherits from bt.Synapse.
+    This protocol handles event graph request and response communication between
+    the miner and the validator.
+    """
+
+    target: typing.Optional[str] = None
+    target_block_number: typing.Optional[int] = None
+
+    subgraph_output: typing.Optional[GraphPayload] = None
