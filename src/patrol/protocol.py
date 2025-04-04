dae5a2a0cc9823df5e3f537cea896b4c91d2592a
Jack Foster <jack@tensora.com>
2025-04-04 10:55:18 +0100

    Updating protocol models

--- src/patrol/protocol.py
+++ src/patrol/protocol.py
@@ -1,5 +1,15 @@
 import typing
 import bittensor as bt
+from dataclasses import dataclass
+from typing import List, Optional
+
+class MinerPingSynapse(bt.Synapse):
+    """
+    A simple ping synapse that inherits from bt.Synapse.
+    This synapse handles ping request and response communication between
+    the miner and the validator.
+    """
+    is_available: typing.Optional[bool] = False
 
 class PatrolSynapse(bt.Synapse):
     """
@@ -11,17 +21,46 @@ class PatrolSynapse(bt.Synapse):
     target: typing.Optional[str] = None
     target_block_number: typing.Optional[int] = None
 
-
     subgraph_output: typing.Optional[dict] = None
 
-    
-class MinerPingSynapse(bt.Synapse):
-    """
-    A simple ping synapse that inherits from bt.Synapse.
-    This synapse handles ping request and response communication between
-    the miner and the validator.
-    """
-    is_available: typing.Optional[bool] = False
+@dataclass
+class TransferEvidence:
+    rao_amount: int
+    block_number: int
 
+@dataclass
+class StakeEvidence:
+    block_number: int
+    rao_amount: int
+    destination_net_uid: int = None
+    source_net_uid: int = None
+    alpha_amount: Optional[int] = None
+    delegate_hotkey_source: Optional[str] = None
+    delegate_hotkey_destination: Optional[str] = None
 
+    def __post_init__(self):
+        if self.destination_net_uid is None and self.source_net_uid is None:
+            raise ValueError("Either destination_net_uid or source_net_uid must be provided.")
+        if self.delegate_hotkey_source is None and self.delegate_hotkey_destination is None:
+            raise ValueError("Either delegate_hotkey_source or delegate_hotkey_destination must be provided.")
+
+@dataclass
+class Edge:
+    coldkey_source: str
+    coldkey_destination: str
+    category: str
+    type: str
+    evidence: TransferEvidence | StakeEvidence
+    coldkey_owner: Optional[str] = None
+
+@dataclass
+class Node:
+    id: str
+    type: str
+    origin: str
+
+@dataclass
+class GraphPayload:
+    nodes: List[Node]
+    edges: List[Edge]
 
