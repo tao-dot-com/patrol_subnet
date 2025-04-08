a0640294ffc7c3cc3440622a6ae2b9a6af4b143d
Richard Flitcroft <richard@echoesconsulting.co.uk>
2025-04-08 23:05:34 +0100

    Change default SQLite dir

--- src/patrol/validation/config.py
+++ src/patrol/validation/config.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import create_async_engine
 NETWORK = os.getenv('NETWORK', "finney")
 NET_UID = int(os.getenv('NET_UID', "81"))
 
-DB_DIR = os.getenv('DB_DIR', "/tmp/sqlite")
+DB_DIR = os.getenv('DB_DIR', "/var/patrol/sqlite")
 DB_URL = os.getenv("DB_URL", f"sqlite+aiosqlite:///{DB_DIR}/patrol.db")
 
 db_engine = create_async_engine(DB_URL)
