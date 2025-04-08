e9850b7e2809dffd74bf59e3b4175fa157985e4c
Richard Flitcroft <richard@echoesconsulting.co.uk>
2025-04-08 12:17:05 +0100

    Remove weight_watcher; add lint checks (commented)

--- build-validator.sh
+++ build-validator.sh
@@ -4,16 +4,15 @@ set -e
 
 docker compose up db --wait
 
+#flake8 ./src --count --select=E9,F63,F7,F82 --show-source --statistics
+# exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
+#flake8 ./src --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
+
 pytest ./tests
 
 docker build \
-  -t tensora/patrol-validator \
+  -t patrol/validator \
   -f validator.dockerfile \
   --build-arg TEST_POSTGRESQL_URL="postgresql+asyncpg://patrol:password@172.17.0.1:5432/patrol" \
   .
 
-docker build \
-  -t tensora/patrol-validator:rds \
-  -f validator_aws_iam.dockerfile \
-  --build-arg TEST_POSTGRESQL_URL="postgresql+asyncpg://patrol:password@172.17.0.1:5432/patrol" \
-  .
