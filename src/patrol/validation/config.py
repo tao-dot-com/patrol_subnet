dac163b612deea0dbe55e46e15e87f41c32dc328
Richard Flitcroft <richard@echoesconsulting.co.uk>
2025-04-11 16:59:20 +0100

    Move creation and wiring of db enging to validator start

--- src/patrol/validation/config.py
+++ src/patrol/validation/config.py
@@ -1,14 +1,11 @@
 import os
 
-from sqlalchemy.ext.asyncio import create_async_engine
 
 NETWORK = os.getenv('NETWORK', "finney")
 NET_UID = int(os.getenv('NET_UID', "81"))
 
 DB_DIR = os.getenv('DB_DIR', "/var/patrol/sqlite")
 DB_URL = os.getenv("DB_URL", f"sqlite+aiosqlite:///{DB_DIR}/patrol.db")
-
-db_engine = create_async_engine(DB_URL)
 
 WALLET_NAME = os.getenv('WALLET_NAME', "default")
 HOTKEY_NAME = os.getenv('HOTKEY_NAME', "default")
