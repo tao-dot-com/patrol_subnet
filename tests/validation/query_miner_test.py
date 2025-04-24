db4b33aab4df25b01b3e24344056b343e60e1cb0
Richard Flitcroft <richard@echoesconsulting.co.uk>
2025-04-24 17:19:21 +0100

    Implement toggle for dashboard syndication

--- tests/validation/query_miner_test.py
+++ tests/validation/query_miner_test.py
@@ -83,7 +83,7 @@ async def test_persist_miner_score(mock_axon, test_wallet):
 
     validator = Validator(
         validation_mechanism, target_generator, scoring_mechanism, miner_score_repository_mock, AsyncMock(DashboardClient), dendrite,
-        metagraph, lambda: batch_id, AsyncMock(WeightSetter), enable_weight_setting=True
+        metagraph, lambda: batch_id, AsyncMock(WeightSetter), enable_weight_setting=True, enable_dashboard_syndication=True
     )
     await validator.query_miner(batch_id, uid, axon.info(), ("bar", 123), max_block_number=None)
 
@@ -151,7 +151,7 @@ async def test_query_miner_batch_and_set_weights(mock_axon, test_wallet):
 
     validator = Validator(
         validation_mechanism, target_generator, scoring_mechanism, miner_score_repository, AsyncMock(DashboardClient),
-        dendrite, metagraph, lambda: batch_id, weight_setter, enable_weight_setting=True,
+        dendrite, metagraph, lambda: batch_id, weight_setter, enable_weight_setting=True, enable_dashboard_syndication=True
     )
 
     await validator.query_miner_batch()
@@ -215,7 +215,7 @@ async def test_query_miner_batch_when_weights_are_not_due(mock_axon, test_wallet
 
     validator = Validator(
         validation_mechanism, target_generator, scoring_mechanism, miner_score_repository, AsyncMock(DashboardClient),
-        dendrite, metagraph, lambda: batch_id, weight_setter, enable_weight_setting=True
+        dendrite, metagraph, lambda: batch_id, weight_setter, enable_weight_setting=True, enable_dashboard_syndication=True
     )
 
     await validator.query_miner_batch()
