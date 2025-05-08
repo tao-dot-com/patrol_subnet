8f48590527cc644434606f9c001a816b42a14886
Richard Flitcroft <richard@echoesconsulting.co.uk>
2025-05-08 17:02:20 +0100

    Add tests for HotkeyOwnershipSynapse

---
+++ src/patrol/validation/hotkey_ownership_task.py
@@ -0,0 +1,20 @@
+from bittensor import AxonInfo, Dendrite
+
+from patrol.protocol import HotkeyOwnershipSynapse
+
+class MinerTaskException(Exception):
+    pass
+
+
+class HotkeyOwnershipTask:
+
+    def __init__(self, dendrite: Dendrite):
+        self._dendrite = dendrite
+
+    async def execute_task(self, miner: AxonInfo, synapse: HotkeyOwnershipSynapse):
+        response = await self._dendrite.call(miner, synapse, timeout=60, deserialize=False)
+
+        if response.is_failure:
+            raise MinerTaskException(f"Error: {response.dendrite.status_message}; status {response.dendrite.status_code}")
+
+        return response
