e94480b3f8b9fba56ca62d071b7a35820ef4dab3
Jack Foster <jack@tensora.com>
2025-05-12 12:40:27 +0100

    Adding additional validation tests

--- tests/validation/hotkey_ownership/hotkey_ownership_validator_test.py
+++ tests/validation/hotkey_ownership/hotkey_ownership_validator_test.py
@@ -12,7 +12,7 @@ async def test_validation_of_valid_graph():
     chain_reader = AsyncMock(ChainReader)
 
     valid_response = HotkeyOwnershipSynapse(
-        target_hotkey_ss58="abcdef", subgraph_output=GraphPayload(
+        target_hotkey_ss58="abcdef", max_block_number=200, subgraph_output=GraphPayload(
             nodes=[
                 Node("alice", type="wallet", origin="bittensor"),
                 Node("bob", type="wallet", origin="bittensor")
@@ -39,7 +39,7 @@ async def test_validation_with_unconnected_node():
     chain_reader = AsyncMock(ChainReader)
 
     valid_response = HotkeyOwnershipSynapse(
-        target_hotkey_ss58="abcdef", subgraph_output=GraphPayload(
+        target_hotkey_ss58="abcdef", max_block_number=200, subgraph_output=GraphPayload(
             nodes=[
                 Node("alice", type="wallet", origin="bittensor"),
                 Node("bob", type="wallet", origin="bittensor"),
@@ -64,7 +64,7 @@ async def test_validation_with_unconnected_edges():
     chain_reader = AsyncMock(ChainReader)
 
     valid_response = HotkeyOwnershipSynapse(
-        target_hotkey_ss58="abcdef", subgraph_output=GraphPayload(
+        target_hotkey_ss58="abcdef", max_block_number=200, subgraph_output=GraphPayload(
             nodes=[
                 Node("alice", type="wallet", origin="bittensor"),
                 Node("bob", type="wallet", origin="bittensor"),
@@ -93,7 +93,7 @@ async def test_validation_with_duplicate_nodes():
     chain_reader = AsyncMock(ChainReader)
 
     valid_response = HotkeyOwnershipSynapse(
-        target_hotkey_ss58="abcdef", subgraph_output=GraphPayload(
+        target_hotkey_ss58="abcdef", max_block_number=200, subgraph_output=GraphPayload(
             nodes=[
                 Node("alice", type="wallet", origin="bittensor"),
                 Node("alice", type="wallet", origin="bittensor"),
@@ -113,7 +113,7 @@ async def test_validation_with_empty_nodes():
     chain_reader = AsyncMock(ChainReader)
 
     valid_response = HotkeyOwnershipSynapse(
-        target_hotkey_ss58="abcdef", subgraph_output=GraphPayload(
+        target_hotkey_ss58="abcdef", max_block_number=200, subgraph_output=GraphPayload(
             nodes=[],
             edges=[]
         ))
@@ -130,7 +130,7 @@ async def test_validation_with_absent_nodes():
     chain_reader = AsyncMock(ChainReader)
 
     valid_response = HotkeyOwnershipSynapse(
-        target_hotkey_ss58="abcdef", subgraph_output=GraphPayload(
+        target_hotkey_ss58="abcdef", max_block_number=200, subgraph_output=GraphPayload(
             nodes=None,
             edges=[]
         ))
@@ -147,7 +147,7 @@ async def test_validation_with_absent_subgraph():
     chain_reader = AsyncMock(ChainReader)
 
     valid_response = HotkeyOwnershipSynapse(
-        target_hotkey_ss58="abcdef", subgraph_output=None
+        target_hotkey_ss58="abcdef", max_block_number=200, subgraph_output=None
     )
 
     validator = HotkeyOwnershipValidator(chain_reader)
@@ -163,7 +163,7 @@ async def test_validation_with_incorrect_previous_owner():
     chain_reader = AsyncMock(ChainReader)
 
     valid_response = HotkeyOwnershipSynapse(
-        target_hotkey_ss58="abcdef", subgraph_output=GraphPayload(
+        target_hotkey_ss58="abcdef", max_block_number=200, subgraph_output=GraphPayload(
             nodes=[
                 Node("alice", type="wallet", origin="bittensor"),
                 Node("bob", type="wallet", origin="bittensor"),
@@ -190,15 +190,20 @@ async def test_validation_with_incorrect_new_owner():
     chain_reader = AsyncMock(ChainReader)
 
     valid_response = HotkeyOwnershipSynapse(
-        target_hotkey_ss58="abcdef", subgraph_output=GraphPayload(
+        target_hotkey_ss58="abcdef", max_block_number=200, subgraph_output=GraphPayload(
             nodes=[
                 Node("alice", type="wallet", origin="bittensor"),
                 Node("bob", type="wallet", origin="bittensor"),
+                Node("carol", type="wallet", origin="bittensor"),
             ],
             edges=[
                 Edge(
                     coldkey_source="alice", coldkey_destination="bob", category="", type="",
                     evidence=HotkeyOwnershipEvidence(123)
+                ),
+                Edge(
+                    coldkey_source="alice", coldkey_destination="carol", category="", type="",
+                    evidence=HotkeyOwnershipEvidence(124)
                 )
             ]
         ))
@@ -212,12 +217,44 @@ async def test_validation_with_incorrect_new_owner():
 
     assert str(ex.value) == "Expected hotkey_owner [bob]; actual [carol] for block [124]"
 
+async def test_validation_with_incorrect_final_owner():
+
+    chain_reader = AsyncMock(ChainReader)
+
+    valid_response = HotkeyOwnershipSynapse(
+        target_hotkey_ss58="abcdef", max_block_number=200, subgraph_output=GraphPayload(
+            nodes=[
+                Node("alice", type="wallet", origin="bittensor"),
+                Node("bob", type="wallet", origin="bittensor"),
+                Node("carol", type="wallet", origin="bittensor"),
+            ],
+            edges=[
+                Edge(
+                    coldkey_source="alice", coldkey_destination="bob", category="", type="",
+                    evidence=HotkeyOwnershipEvidence(123)
+                ),
+                Edge(
+                    coldkey_source="bob", coldkey_destination="carol", category="", type="",
+                    evidence=HotkeyOwnershipEvidence(124)
+                )
+            ]
+        ))
+
+    chain_reader.get_hotkey_owner = AsyncMock(side_effect=lambda hk, bn: "alice" if bn < 123 else "bob")
+
+    validator = HotkeyOwnershipValidator(chain_reader)
+
+    with pytest.raises(ValidationException) as ex:
+        await validator.validate(valid_response, "abddef12345")
+    # Actually caught by a different error
+    assert str(ex.value) == "Expected hotkey_owner [carol]; actual [bob] for block [125]"
+
 async def test_validation_with_duplicate_edge():
 
     chain_reader = AsyncMock(ChainReader)
 
     valid_response = HotkeyOwnershipSynapse(
-        target_hotkey_ss58="abcdef", subgraph_output=GraphPayload(
+        target_hotkey_ss58="abcdef", max_block_number=200, subgraph_output=GraphPayload(
             nodes=[
                 Node("alice", type="wallet", origin="bittensor"),
                 Node("bob", type="wallet", origin="bittensor"),
@@ -246,30 +283,37 @@ async def test_validation_with_no_changes_of_ownership_since_lowest_block_in_ran
     chain_reader = AsyncMock(ChainReader)
 
     valid_response = HotkeyOwnershipSynapse(
-        target_hotkey_ss58="abcdef", subgraph_output=GraphPayload(
+        target_hotkey_ss58="abcdef", max_block_number=200, subgraph_output=GraphPayload(
             nodes=[
                 Node("alice", type="wallet", origin="bittensor"),
             ],
             edges=[]
         ))
+    
+    chain_reader.get_hotkey_owner = AsyncMock(side_effect=lambda hk, bn: "alice" if bn < 201 else "alice")
 
     validator = HotkeyOwnershipValidator(chain_reader)
 
-    await validator.validate(valid_response, "abddef12345")
+    await validator.validate(valid_response, "abcdef")
 
-async def test_validation_with_no_changes_of_ownership():
+async def test_validation_target_hotkey_not_linked_to_graph():
 
     chain_reader = AsyncMock(ChainReader)
 
     valid_response = HotkeyOwnershipSynapse(
-        target_hotkey_ss58="abcdef", subgraph_output=GraphPayload(
+        target_hotkey_ss58="abcdef", max_block_number=200, subgraph_output=GraphPayload(
             nodes=[
                 Node("alice", type="wallet", origin="bittensor"),
             ],
             edges=[]
         ))
+    
+    chain_reader.get_hotkey_owner = AsyncMock(side_effect=lambda hk, bn: "bob" if bn < 201 else "carol")
 
     validator = HotkeyOwnershipValidator(chain_reader)
 
-    await validator.validate(valid_response, "abddef12345")
+    with pytest.raises(ValidationException) as ex:
+        await validator.validate(valid_response, "abddef12345")
+
+    assert str(ex.value) == "Start owner [carol] is not in the graph"
 
