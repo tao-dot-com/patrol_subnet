25e5909a97155bdcb873fcfa6274b719e24cde91
David Marsden <david@tensora.com>
2025-05-21 17:43:22 +0100

    feat: added ChainStakeEvent dataclass

--- src/patrol/validation/predict_alpha_sell/__init__.py
+++ src/patrol/validation/predict_alpha_sell/__init__.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from enum import Enum
 from uuid import UUID
+from typing import Optional
 
 class TransactionType(Enum):
     UNSTAKE = "UNSTAKE"
@@ -36,3 +37,19 @@ class AlphaSellChallengeRepository(ABC):
     @abstractmethod
     def add(self, challenge):
         pass
+
+@dataclass(frozen=True)
+class ChainStakeEvent:
+    created_at: datetime
+    edge_category: str
+    block_number: int
+    edge_type: str
+    coldkey_destination: Optional[str] = None
+    coldkey_source: Optional[str] = None
+    coldkey_owner: Optional[str] = None
+    rao_amount: Optional[int] = None
+    destination_net_uid: Optional[int] = None
+    source_net_uid: Optional[int] = None
+    alpha_amount: Optional[int] = None
+    delegate_hotkey_source: Optional[str] = None
+    delegate_hotkey_destination: Optional[str] = None
